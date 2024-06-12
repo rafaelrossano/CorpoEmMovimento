82e6537de03d4c770ecc05d98c50fc45d2265bb0
Rafael Rossano <rafaelrossanopo@gmail.com>
2024-06-12 19:52:11 -0300

    Create New App

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Teste string")
+   
+   
+def sobre(request):
+    return HttpResponse("Sobre mim...") 
+
+
+def contato(request):
+    return HttpResponse("Contatos...")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home), #Home
+    path('sobre/', sobre), #sobre
+    path('contato/', contato) #contato
 ]
