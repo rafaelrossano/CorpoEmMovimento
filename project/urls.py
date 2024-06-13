0ab0ec40ecd65640ce304aea959b70837a9914ff
Rafael Rossano <rafaelrossanopo@gmail.com>
2024-06-13 12:33:41 -0300

    Craeting templates

--- project/urls.py
+++ project/urls.py
@@ -15,24 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Teste string")
-   
-   
-def sobre(request):
-    return HttpResponse("Sobre mim...") 
-
-
-def contato(request):
-    return HttpResponse("Contatos...")
-
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home), #Home
-    path('sobre/', sobre), #sobre
-    path('contato/', contato) #contato
+    path('', include('recipes.url')),
 ]
